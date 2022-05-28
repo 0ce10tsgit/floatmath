2a81073524de78f873b92402539a955e7da2eca3
0ce10t <96030922+0ce10tsgit@users.noreply.github.com>
2022-05-28 19:42:20 -0400

    Clean up

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# floatmath
-made this  cus 2.3 x 100 is not equal to. 229.999997
-functions are:  add, subtract, multiply, divide, mod 
-at the moment they only do 2 arguments and decimals with a large size *might* overflow
-just do "import * as floatmath from './ftmath.js'
+Just take the file copy pasta it and include it in < script>
+and then\
+"let namethiswhatever = new PlsDontBeDumb()"\
+Methods are \
+.subtract(num1,num2) \
+.add(num1,num2)\
+.multiply(num1,num2)\
+.divide(num1,num2)\
+mod is wip cus Idk if the error happens there
+\
+how it works:
+\
+Any decimal operation can be recreated with non decimals eg 0.1 + 0.1 = 0.2 and 1 + 1 = 2\
+so the library calculates scalar that will turn both digits into whole numbers if they arent already whole numbers\
+then it just does the operation with the scaled up version and uses the scalar to bring the result down to a decimal\
+however this entire solution uses division and multiplication which is where the problem is at in the first place\
+so I used strings to solve that, the number is turned to a string then an array and then the numbers are scaled up by anaylizing the location of the '.'\ 
+the same process is used to lower the scaled up product to the true product.\
+
+
--- ftmath.js
+++ ftmath.js
@@ -1,5 +1,5 @@
-// No markup cus I killed all my braincells doing this I just used the string lengts before and after the . to manually move the decimal place so I could scale without float bullshit
-function correct(f1,f2){
+class F{
+  #correct(f1,f2){
   let rl1;
   let rl2;
   if (f1.toString().includes('.')){
@@ -15,171 +15,161 @@ function correct(f1,f2){
     rl2 = f2.toString() + '.0'
   }
   return [rl1,rl2]
-}
-export function multiply(f1,f2){
-  let first = correct(f1,f2)[0]
-  let second = correct(f1,f2)[1]
-  if (first.split('.')[1].length == second.split('.')[1].length){
-    let newbalancer = '1'
-    for (let i = 0; i < first.split('.')[1].length; i++){
-      newbalancer = newbalancer + '0'
-    }
-    newbalancer = parseInt(newbalancer)
-    let expanded = parseInt(first.replace('.',''))*parseInt(second.replace('.',''))
-    return expanded/(newbalancer*newbalancer)
-  }else{
-    let balancer = '1'
-    let amount = 0
-    let larger;
-    let smoler;
-    if (first.split('.')[1].length > second.split('.')[1].length){
-      larger = first
-      smoler = second
-    }
-    else{
-      larger = second
-      smoler = first
-    }
-    for(let x = 0; x < larger.split('.')[1].length; x++){
-      balancer = balancer + '0'
-      amount++
-    }
-    let offset = smoler.split('.')[1].length
-    smoler = smoler.replace('.','')
-    for(let z = 0; z <  (amount - offset); z++){
-      smoler = smoler + '0'
-    }
-    let expanded = parseInt(smoler) * parseInt(larger.replace('.',''))
-    return expanded/(parseInt(balancer)*parseInt(balancer))
   }
-}
-
-export function divide(f1,f2){
-  let first = correct(f1,f2)[0]
-  let second = correct(f1,f2)[1]
-  if (first.split('.')[1].length == second.split('.')[1].length){
-    let expanded = parseInt(first.replace('.',''))/parseInt(second.replace('.',''))
-    return expanded
-  }else{
-    let amount = 0
-    let larger;
-    let smoler;
-    let reg_order;
-    if (first.split('.')[1].length > second.split('.')[1].length){
-      larger = first
-      smoler = second
-      reg_order = true
+  multiply(f1,f2){
+    let [first,second] = this.#correct(f1,f2)
+    if (first.split('.')[1].length == second.split('.')[1].length){
+      let newbalancer = '1'
+      for (let i = 0; i < first.split('.')[1].length; i++){
+        newbalancer = newbalancer + '0'
+      }
+      newbalancer = parseInt(newbalancer)
+      let expanded = parseInt(first.replace('.',''))*parseInt(second.replace('.',''))
+      return expanded/(newbalancer*newbalancer)
+    }else{
+      let balancer = '1'
+      let amount = 0
+      let smoler,larger;
+      if (first.split('.')[1].length > second.split('.')[1].length){
+        larger = first
+        smoler = second
+      }
+      else{
+        larger = second
+        smoler = first
+      }
+      for(let x = 0; x < larger.split('.')[1].length; x++){
+        balancer = balancer + '0'
+        amount++
+      }
+      let offset = smoler.split('.')[1].length
+      smoler = smoler.replace('.','')
+      for(let z = 0; z <  (amount - offset); z++){
+        smoler = smoler + '0'
+      }
+      let expanded = parseInt(smoler) * parseInt(larger.replace('.',''))
+      return expanded/(parseInt(balancer)*parseInt(balancer))
     }
-    else{
-      larger = second
-      smoler = first
-      reg_order = false
-    }
-    for(let x = 0; x < larger.split('.')[1].length; x++){
-      amount++
-    }
-    let offset = smoler.split('.')[1].length
-    smoler = smoler.replace('.','')
-    for(let z = 0; z <  (amount - offset); z++){
-      smoler = smoler + '0'
-    }
-    let expanded;
-    if (reg_order == false){
-      expanded = parseInt(smoler) / parseInt(larger.replace('.',''))
-    }
-    else{
-      expanded = parseInt(larger.replace('.','')) / parseInt(smoler)
-    }
-    return expanded
   }
-}
-export function add(f1,f2){
-  let first = correct(f1,f2)[0]
-  let second = correct(f1,f2)[1]
-  if (first.split('.')[1].length == second.split('.')[1].length){
-    let newbalancer = '1'
-    for (let i = 0; i < first.split('.')[1].length; i++){
-      newbalancer = newbalancer + '0'
+  divide(f1,f2){
+    let [first,second] = this.#correct(f1,f2)
+    if (first.split('.')[1].length == second.split('.')[1].length){
+      let expanded = parseInt(first.replace('.',''))/parseInt(second.replace('.',''))
+      return expanded
+    }else{
+      let amount = 0
+      let larger,smoler,reg_order
+      if (first.split('.')[1].length > second.split('.')[1].length){
+        larger = first
+        smoler = second
+        reg_order = true
+      }
+      else{
+        larger = second
+        smoler = first
+        reg_order = false
+      }
+      for(let x = 0; x < larger.split('.')[1].length; x++){
+        amount++
+      }
+      let offset = smoler.split('.')[1].length
+      smoler = smoler.replace('.','')
+      for(let z = 0; z <  (amount - offset); z++){
+        smoler = smoler + '0'
+      }
+      let expanded;
+      if (reg_order == false){
+        expanded = parseInt(smoler) / parseInt(larger.replace('.',''))
+      }
+      else{
+        expanded = parseInt(larger.replace('.','')) / parseInt(smoler)
+      }
+      return expanded
     }
-    newbalancer = parseInt(newbalancer)
-    let expanded = parseInt(first.replace('.',''))+parseInt(second.replace('.',''))
-    return expanded/parseInt(newbalancer)
-  }else{
-    let balancer = '1'
-    let amount = 0
-    let larger;
-    let smoler;
-    if (first.split('.')[1].length > second.split('.')[1].length){
-      larger = first
-      smoler = second
-    }
-    else{
-      larger = second
-      smoler = first
-    }
-    for(let x = 0; x < larger.split('.')[1].length; x++){
-      balancer = balancer + '0'
-      amount++
-    }
-    let offset = smoler.split('.')[1].length
-    smoler = smoler.replace('.','')
-    for(let z = 0; z <  (amount - offset); z++){
-      smoler = smoler + '0'
-    }
-    let expanded = parseInt(smoler) + parseInt(larger.replace('.',''))
-    return expanded/parseInt(balancer)
   }
-}
-export function subtract(f1,f2){
-  let first = correct(f1,f2)[0]
-  let second = correct(f1,f2)[1]
-  if (first.split('.')[1].length == second.split('.')[1].length){
-    let balancer = '1'
-    for (let i = 0; i < first.split('.')[1].length; i++){
-      balancer = balancer + '0'
+  add(f1,f2){
+   let [first,second] = this.#correct(f1,f2)
+    if (first.split('.')[1].length == second.split('.')[1].length){
+      let newbalancer = '1'
+      for (let i = 0; i < first.split('.')[1].length; i++){
+        newbalancer = newbalancer + '0'
+      }
+      newbalancer = parseInt(newbalancer)
+      let expanded = parseInt(first.replace('.',''))+parseInt(second.replace('.',''))
+      return expanded/parseInt(newbalancer)
+    }else{
+      let balancer = '1'
+      let amount = 0
+      let larger,smoler;
+      if (first.split('.')[1].length > second.split('.')[1].length){
+        larger = first
+        smoler = second
+      }
+      else{
+        larger = second
+        smoler = first
+      }
+      for(let x = 0; x < larger.split('.')[1].length; x++){
+        balancer = balancer + '0'
+        amount++
+      }
+      let offset = smoler.split('.')[1].length
+      smoler = smoler.replace('.','')
+      for(let z = 0; z <  (amount - offset); z++){
+        smoler = smoler + '0'
+      }
+      let expanded = parseInt(smoler) + parseInt(larger.replace('.',''))
+      return expanded/parseInt(balancer)
     }
-    let expanded = parseInt(first.replace('.',''))-parseInt(second.replace('.',''))
-    return expanded/parseInt(balancer)
-  }else{
-    let amount = 0
-    let larger;
-    let smoler;
-    let reg_order;
-    if (first.split('.')[1].length > second.split('.')[1].length){
-      larger = first
-      smoler = second
-      reg_order = true
-    }
-    else{
-      larger = second
-      smoler = first
-      reg_order = false
-    }
-    for(let x = 0; x < larger.split('.')[1].length; x++){
-      amount++
-    }
-    let offset = smoler.split('.')[1].length
-    smoler = smoler.replace('.','')
-    for(let z = 0; z <  (amount - offset); z++){
-      smoler = smoler + '0'
-    }
-    let expanded;
-    let balancer = '1'
-    for(let p = 0; p < larger.split('.')[1].length;p++){
-      balancer = balancer + '0'
-    }
-    if (reg_order == false){
-      expanded = parseInt(smoler) - parseInt(larger.replace('.',''))
-    }
-    else{
-      expanded = parseInt(larger.replace('.','')) - parseInt(smoler)
-    }
-    return expanded/parseInt(balancer)
   }
-}
+  subtract(f1,f2){
+    let [first,second] = this.#correct(f1,f2)
+    if (first.split('.')[1].length == second.split('.')[1].length){
+      let balancer = '1'
+      for (let i = 0; i < first.split('.')[1].length; i++){
+        balancer = balancer + '0'
+      }
+      let expanded = parseInt(first.replace('.',''))-parseInt(second.replace('.',''))
+      return expanded/parseInt(balancer)
+    }else{
+      let amount = 0
+      let larger,smoler,reg_order;
+      if (first.split('.')[1].length > second.split('.')[1].length){
+        larger = first
+        smoler = second
+        reg_order = true
+      }
+      else{
+        larger = second
+        smoler = first
+        reg_order = false
+      }
+      for(let x = 0; x < larger.split('.')[1].length; x++){
+        amount++
+      }
+      let offset = smoler.split('.')[1].length
+      smoler = smoler.replace('.','')
+      for(let z = 0; z <  (amount - offset); z++){
+        smoler = smoler + '0'
+      }
+      let expanded;
+      let balancer = '1'
+      for(let p = 0; p < larger.split('.')[1].length;p++){
+        balancer = balancer + '0'
+      }
+      if(reg_order == false){
+        expanded = parseInt(smoler) - parseInt(larger.replace('.',''))
+      }
+      else{
+        expanded = parseInt(larger.replace('.','')) - parseInt(smoler)
+      }
+      return expanded/parseInt(balancer)
+    }
+  }
+/*
 export function mod(f1,f2){
-  let first = correct(f1,f2)[0]
-  let second = correct(f1,f2)[1]
+  let first = this.#correct(f1,f2)[0]
+  let second = this.#correct(f1,f2)[1]
   if (first.split('.')[1].length == second.split('.')[1].length){
     let newbalancer = '1'
     for (let i = 0; i < first.split('.')[1].length; i++){
@@ -213,4 +203,7 @@ export function mod(f1,f2){
     let expanded = parseInt(smoler) % parseInt(larger.replace('.',''))
     return expanded/(parseInt(balancer)*parseInt(balancer))
   }
+}0
+*/ 
+//broken af ^^^
 }
